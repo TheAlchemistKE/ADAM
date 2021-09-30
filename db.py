67fdcaacb36964742ac03d41be7ff08d113c74e2
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-09-30 22:44:45 +0300

    Add code to create db and tables

--- db.py
+++ db.py
@@ -1,13 +1,23 @@
 import sqlite3
 from datetime import datetime
 
-DB_NAME='2021-06'
+timeframe='2021-06'
 
-conn = sqlite3.connect('{}.db', format=DB_NAME)
+conn = sqlite3.connect('{}.db'.format(timeframe))
 cur = conn.cursor()
 
 def create_table():
-    pass
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS parent_reply (
+        parent_id TEXT PRIMARY KEY,
+        comment_id TEXT UNIQUE,
+        parent TEXT,
+        comment TEXT,
+        subreddit TEXT,
+        unix INT,
+        score INT
+    )
+    """)
 
 if __name__ == '__main__':
     create_table()
