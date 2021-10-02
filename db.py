834db0d52c14337aace7039e876c3a41dc775480
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-10-03 02:05:03 +0300

    Add cleanup db utilities.

--- db.py
+++ db.py
@@ -6,6 +6,9 @@ timeframe='2021-06'
 
 conn = sqlite3.connect('{}.db'.format(timeframe))
 cur = conn.cursor()
+sql_transaction = []
+start_row = 0
+cleanup = 1000000
 
 def create_table():
     cur.execute("""
@@ -23,12 +26,15 @@ def create_table():
 def format_data(data: str):
     data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
     return data
-def acceptable(data: str):
-    if len(data.split(' ')) > 50 or len(data) < 1:
+
+def acceptable(data):
+    if len(data.split(' ')) > 1000 or len(data) < 1:
         return False
-    elif len(data) > 1000:
+    elif len(data) > 32000:
         return False
-    elif data == '[deleted]' or data == '[removed]':
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
         return False
     else:
         return True
@@ -61,14 +67,40 @@ def find_parent(pid: str):
         # print('find_parent', e)
         return False
 
-def sql_insert_replace(cid, pid, data, body, subred, created, score):
-    pass
 
-def sql_insert_has_parent(cid, pid, data, body, subred, created, score):
-    pass
+def transaction_bldr(sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        cur.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                cur.execute(s)
+            except:
+                pass
+        conn.commit()
+        sql_transaction = []
 
-def sql_insert_no_parent(cid, pid, body, subred, created, score):
-    pass
+def sql_insert_replace(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
+
+def sql_insert_has_parent(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(parentid, commentid, parent, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
+
+def sql_insert_no_parent(commentid,parentid,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(parentid, commentid, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
 
 
 if __name__ == '__main__':
@@ -83,6 +115,7 @@ if __name__ == '__main__':
             body = format_data(row['body'])
             created_utc = row['created_utc']
             score = row['score']
+            comment_id = row['name']
             subreddit = row['subreddit']
             parent_data = find_parent(parent_id)
 
@@ -97,4 +130,16 @@ if __name__ == '__main__':
                     sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
                 else: 
                     sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
+            
+            if row_counter % 100000 == 0:
+                print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows, str(datetime.now())))
+
+            if row_counter > start_row:
+                if row_counter % cleanup == 0:
+                    print("Cleanin up!")
+                    sql = "DELETE FROM parent_reply WHERE parent IS NULL"
+                    cur.execute(sql)
+                    conn.commit()
+                    cur.execute("VACUUM")
+                    conn.commit()
 
