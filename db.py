5e9815606526f383a1bdc42266dbd61188750830
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-10-02 17:58:34 +0300

    Add structure and criteria for acceptable data

--- db.py
+++ db.py
@@ -23,6 +23,30 @@ def create_table():
 def format_data(data: str):
     data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
     return data
+def acceptable(data: str):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return True
+
+
+
+def find_existing_score(pid: str):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        cur.execute(sql)
+        result = cur.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return None
+    except Exception as e:
+        # print('find_parent', e)
+        return False
 
 def find_parent(pid: str):
     try:
@@ -36,15 +60,22 @@ def find_parent(pid: str):
     except Exception as e:
         # print('find_parent', e)
         return False
-    
 
+def sql_insert_replace():
+    pass
+
+def sql_insert_has_parent():
+    pass
+
+def sql_insert_no_parent():
+    pass
 
 
 if __name__ == '__main__':
     create_table()
     row_counter = 0
     paired_rows = 0
-    with open('/home/kelyn/Downloads/AdamDataset/RC_2021-06', buffer=1000) as f:
+    with open('/home/kelyn/Downloads/AdamDataset/RC_2021-06', buffering=1000) as f:
         for row in f:
             row_counter += 1
             row = json.loads(row)
@@ -53,4 +84,17 @@ if __name__ == '__main__':
             created_utc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+
+            if score > 10:
+                if acceptable(body):
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            sql_insert_replace(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+            else:
+                if parent_data:
+                    sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+                else: 
+                    sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
 
